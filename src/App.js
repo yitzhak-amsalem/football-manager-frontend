39fd20e196c9075b24b7217c011907137a96b271
Yitzhak Amsalem <yitzhak@gmail.com>
2022-12-14 13:48:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {sendApiGetRequest, sendApiPostRequest} from "./ApiRequests";
+import Wrapper from "./Wrapper";
+
+function App() {
+
+
+
+    return (
+        <div className="App">
+            <Wrapper/>
+        </div>
+    );
+}
+
+export default App;
